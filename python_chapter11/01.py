9b5e396a45e0ca43149c86b15f6336bf041d7f2a
노희주 <https://github.com/hiju200208-png/ai-data-analysis.git>
2026-08-24 16:58:50 +0900

    02.py추가

---
+++ python_chapter11/01.py
@@ -0,0 +1,26 @@
+colors = ["red", "green", "blue", "yellow", "black"]
+
+print(colors[0])
+#red
+print(colors[2])
+#blue
+print(colors[-1])
+#black
+print(colors[-2])
+#yellow
+print(colors[1:4])
+#['green','blue','yellow']
+print(colors[:3])
+#['red','green','blue']
+print(colors[3:])
+#['yellow','black']
+
+#첫 번째 값
+print(colors[0])
+
+#마지막 값
+print(colors[-1])
+#가운데 세 값
+print(colors[1:4])
+#마지막 두 값
+print(colors[3:])
